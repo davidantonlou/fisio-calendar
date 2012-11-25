4ef35d7ea6ff1f770ca52eb6ad5593599c90f35f
joseluisalcalaramos@gmail.com <joseluisalcalaramos@gmail.com@1f00ed70-09a5-b7cf-bc8c-4d3ba086ca95>
2012-11-25 20:01:37 +0000

    nnuevos estilos

--- branches/FisioMaquet/WebContent/reservar_cita.php
+++ branches/FisioMaquet/WebContent/reservar_cita.php
@@ -14,46 +14,38 @@
     <meta charset="utf-8" />
     <title>Fisioterapia Valdespartera</title>
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
+    <link rel="stylesheet" href="css/calendarStyle.css"> 
     <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
     <script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
     <script src="http://jquery-ui.googlecode.com/svn/trunk/ui/i18n/jquery.ui.datepicker-es.js"></script>
     <script src="js/users_calendars.js"></script>
     <script src="js/calendar.js"></script>
 
-    <style type="text/css">
-    	div.ui-datepicker{
-    	font-size:10px;
-    	}
- 		div.background{
- 			background-image:url('http://www.fisioterapiavaldespartera.com/sites/all/themes/danland/images/fondo-content.jpg');
- 		}
- 		body{
- 		color: #000000;
-	    font-family: Verdana,Arial,Helvetica,sans-serif;
-	    font-size: 84%;
-	    line-height: 1.5em;
-       }
-    </style>
-    	
      <script>
 	    $(function() {
 	        $( "#datepicker" ).datepicker($.datepicker.regional['es']);
-	    });
-	    
+	    });	    
      </script>
-  	 <script src="js/calendarActions.js"></script>
-  	 
-     <script src="https://apis.google.com/js/client.js?onload=handleClientLoad"></script>
+     
+  	 <script src="js/calendarActions.js"></script>  	 
+     <script src="https://apis.google.com/js/client.js"></script>
+     
 </head>
-<body onLoad="javascript:handleAuthClick();">
+<body>
 <div class="background">
-<input type="button" value="Desconectar" onclick="javascript: location.href='login.php?modo=desconectar';"/></td>
-<h1>Reserva de citas</h1>
+
+<a class="button" href="javascript: location.href='login.php?modo=desconectar';" style="margin-left: 90%">Desconectar</a>
+
+<h1 class="title">Reserva de citas</h1>
 <form name="createEvent" method="post" action="confirmar_reserva.php">
 	<input type="hidden" name="calendar"/>
-	 <table>
+	<br/><br/>
+	 <table style="padding-left: 10%">
 	 	<tr>
-	 		<td>Seleccione doctor :
+	 		<td>
+	 			<span class="text">Seleccione doctor :</span>
+	 		</td>
+	 		<td>
 	 			<select id="calendarCombo" onchange="changeCalendar(this.value);">
 	 				<option selected="selected" value="0">-- Selecciona un Fisio</option>
 	 				<option value="1">Pablo</option>
@@ -62,20 +54,36 @@
 	 			</select>
 	 		</td>
 	 	</tr>
-	 	<tr width="50px">
-	 		<td> Fecha: <input type="text" id="datepicker" name="datepicker"/> Hora: <select  id="selectDate" name="selectDate" disabled="disabled"/> </td>
+	 	<tr>
+	 		<td> 
+	 			<span class="text">Fecha:</span>
+	 		</td>
+	 		<td>
+		 		<input type="text" id="datepicker" name="datepicker"/> 		 		
+	 		</td>
+	 		
 	 	</tr>
 	 	<tr>
-	 		<td> Nombre: <input type="text" size="55px" id="title" name="title"/> </td>
+	 		<td>
+	 			<span class="text">Hora:</span>  
+	 		</td>
+	 		<td>
+	 			<select  id="selectDate" name="selectDate" disabled="disabled"/>
+	 		</td>
 	 	</tr>
 	 	<tr>
-	 		<td> Anotaciones adicionales: <textarea id="description" name="description" style="resize: none;" cols="25" rows="2"></textarea>
+	 		<td> <span class="text">Nombre: </span>  </td>
+	 		<td> <input type="text" size="55px" id="title" name="title"/> </td>
 	 	</tr>
 	 	<tr>
-	 		<td><input type="submit" value="Reservar" name="isReserva" id="isReserva" onclick=""/></td>
+	 		<td> <span class="text"> Anotaciones adicionales: </span> </td>
+	 		<td> <textarea id="description" name="description" style="resize: none;" cols="25" rows="2"></textarea> </td>
 	 	</tr>
 	 </table>
+	 <br/><br/>
+	 <a style="margin-left:45%" class="button" onclick="">Reservar</a>
  </form>
+ <br/><br/>
  <table>
  	<tr>
  		<td><iframe id="doctorCalendar" src="https://www.google.com/calendar/embed?title=Fisioterapia%20Valdespartera&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=hv5gfg41m9g2a87kr27uso10p0%40group.calendar.google.com&amp;color=%23B1440E&amp;src=t82rb1fmjt84v4mq2sso91fr7c%40group.calendar.google.com&amp;color=%232F6309&amp;src=fisiocalendar%40gmail.com&amp;color=%232952A3&amp;ctz=Europe%2FMadrid" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe></td>
