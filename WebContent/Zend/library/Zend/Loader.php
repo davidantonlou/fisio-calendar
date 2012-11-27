db5762d17fb1e30cc0f168e04af6c0f383e2dbff
joseluisalcalaramos@gmail.com <joseluisalcalaramos@gmail.com@1f00ed70-09a5-b7cf-bc8c-4d3ba086ca95>
2012-11-27 17:22:04 +0000

    Arcivbos del zend

--- WebContent/Zend/library/Zend/Loader.php
+++ WebContent/Zend/library/Zend/Loader.php
@@ -16,7 +16,7 @@
  * @package    Zend_Loader
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id$
+ * @version    $Id: Loader.php 24593 2012-01-05 20:35:02Z matthew $
  */
 
 /**
@@ -177,6 +177,9 @@ class Zend_Loader
 
         foreach (self::explodeIncludePath() as $path) {
             if ($path == '.') {
+                if (is_readable($filename)) {
+                    return true;
+                }
                 continue;
             }
             $file = $path . '/' . $filename;
