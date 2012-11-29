18d5e4505ec036c42c5a7808f850f896b91798ca
joseluisalcalaramos@gmail.com <joseluisalcalaramos@gmail.com@1f00ed70-09a5-b7cf-bc8c-4d3ba086ca95>
2012-11-29 20:43:53 +0000

    listado correcto

--- branches/FisioMaquet/WebContent/js/calendarActions.js
+++ branches/FisioMaquet/WebContent/js/calendarActions.js
@@ -8,10 +8,10 @@
     function changeCalendar(value)
     {
     	// Opci—n 0 -> Todos calendarios por defecto
-    	if(value == 0)document.getElementById("doctorCalendar").src="https://www.google.com/calendar/embed?title=Fisioterapia%20Valdespartera&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=hv5gfg41m9g2a87kr27uso10p0%40group.calendar.google.com&amp;color=%23B1440E&amp;src=t82rb1fmjt84v4mq2sso91fr7c%40group.calendar.google.com&amp;color=%232F6309&amp;src=fisiocalendar%40gmail.com&amp;color=%232952A3&amp;ctz=Europe%2FMadrid";
-    	if(value == 1)document.getElementById("doctorCalendar").src="https://www.google.com/calendar/embed?src=fisiocalendar%40gmail.com&ctz=Europe/Madrid";
-    	if(value == 2)document.getElementById("doctorCalendar").src="https://www.google.com/calendar/embed?src=hv5gfg41m9g2a87kr27uso10p0%40group.calendar.google.com&ctz=Europe/Madrid";
-    	if(value == 3)document.getElementById("doctorCalendar").src="https://www.google.com/calendar/embed?src=t82rb1fmjt84v4mq2sso91fr7c%40group.calendar.google.com&ctz=Europe/Madrid";
+    	if(value == 0)document.getElementById("calendarCombo").src="https://www.google.com/calendar/embed?title=Fisioterapia%20Valdespartera&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=hv5gfg41m9g2a87kr27uso10p0%40group.calendar.google.com&amp;color=%23B1440E&amp;src=t82rb1fmjt84v4mq2sso91fr7c%40group.calendar.google.com&amp;color=%232F6309&amp;src=fisiocalendar%40gmail.com&amp;color=%232952A3&amp;ctz=Europe%2FMadrid";
+    	if(value == 1)document.getElementById("calendarCombo").src="https://www.google.com/calendar/embed?src=fisiocalendar%40gmail.com&ctz=Europe/Madrid";
+    	if(value == 2)document.getElementById("calendarCombo").src="https://www.google.com/calendar/embed?src=hv5gfg41m9g2a87kr27uso10p0%40group.calendar.google.com&ctz=Europe/Madrid";
+    	if(value == 3)document.getElementById("calendarCombo").src="https://www.google.com/calendar/embed?src=t82rb1fmjt84v4mq2sso91fr7c%40group.calendar.google.com&ctz=Europe/Madrid";
     	
     	//Restablecemos los campos
     	if(value!=0)document.getElementById("datepicker").disabled = false;  
@@ -36,9 +36,9 @@
 		dateObject.month = day.split("/")[1];
 		dateObject.year = day.split("/")[2];
 		
-		if(document.createEvent.calendar.value == 1) dateObject.calendar = "fisiocalendar@gmail.com";
-		if(document.createEvent.calendar.value == 2) dateObject.calendar = "hv5gfg41m9g2a87kr27uso10p0@group.calendar.google.com";
-		if(document.createEvent.calendar.value == 3) dateObject.calendar = "t82rb1fmjt84v4mq2sso91fr7c@group.calendar.google.com";
+		if(document.createEvent.calendarCombo.value == 1) dateObject.calendar = "fisiocalendar@gmail.com";
+		if(document.createEvent.calendarCombo.value == 2) dateObject.calendar = "hv5gfg41m9g2a87kr27uso10p0@group.calendar.google.com";
+		if(document.createEvent.calendarCombo.value == 3) dateObject.calendar = "t82rb1fmjt84v4mq2sso91fr7c@group.calendar.google.com";
 		
 		return dateObject;
     }
---
+++ branches/FisioMaquet/WebContent/js/validations.js
@@ -0,0 +1,5 @@
+/*
+ * 
+ * JS que valida los datos del formulario, asi evitamos mandar datos erroneos la servidor
+ * 
+ */
