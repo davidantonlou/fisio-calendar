20e5338f1c3c17182a49591f7f4dd84f8758be97
joseluisalcalaramos@gmail.com <joseluisalcalaramos@gmail.com@1f00ed70-09a5-b7cf-bc8c-4d3ba086ca95>
2012-12-02 22:31:05 +0000

    Cambios de estilos y bbdd

--- WebContent/sql/create_bbdd.sql
+++ WebContent/sql/create_bbdd.sql
@@ -1,2 +1,14 @@
+/*Creamos la tabla de los fisios*/
 CREATE TABLE usuarios (id int(8), user varchar(100), pass varchar(100))
+
+/*Creamos la tabla de los pedidos*/
 CREATE TABLE pedidos (numpedido int(10), fecha varchar(20), hora varchar(20), pagado boolean, fisio varchar(40))
+
+/*Insert para los usuarios fisios*/
+INSERT INTO USUARIOS VALUES (1,"Pablo_Fisio",SHA1("pass"));
+
+
+/*Creamos las restricciones y los indices para la tabla*/
+ALTER TABLE PEDIDOS ADD UNIQUE (NUMPEDIDO,FECHA,HORA,PAGADO,FISIO)
+CREATE INDEX INDEX_NUMPEDIDO ON PEDIDOS (NUMPEDIDO(10))
+
