f776eb3f52235d7a4707ca511d7729ec7c18db85
joseluisalcalaramos@gmail.com <joseluisalcalaramos@gmail.com@1f00ed70-09a5-b7cf-bc8c-4d3ba086ca95>
2012-12-03 18:09:27 +0000

    SQL CAMBIOS

--- branches/FisioMaquet/WebContent/sql/create_bbdd.sql
+++ branches/FisioMaquet/WebContent/sql/create_bbdd.sql
@@ -1,14 +1,14 @@
 /*Creamos la tabla de los fisios*/
-CREATE TABLE usuarios (id int(8), user varchar(100), pass varchar(100))
+CREATE TABLE USUARIOS (ID int(8) NOT NULL AUTO_INCREMENT, USER_FISIO varchar(100) NOT NULL, PASS varchar(100) NOT NULL,PRIMARY KEY (ID));
 
 /*Creamos la tabla de los pedidos*/
-CREATE TABLE pedidos (numpedido int(10), fecha varchar(20), hora varchar(20), pagado boolean, fisio varchar(40))
+CREATE TABLE PEDIDOS (NUMPEDIDO int(10) NOT NULL AUTO_INCREMENT, FECHA varchar(20) NOT NULL, HORA varchar(20) NOT NULL, pagado boolean NOT NULL, fisio varchar(40) NOT NULL,PRIMARY KEY(NUMPEDIDO));
 
 /*Insert para los usuarios fisios*/
-INSERT INTO USUARIOS VALUES (1,"Pablo_Fisio",SHA1("pass"));
+INSERT INTO USUARIOS (USER_FISIO,PASS) VALUES ("Pablo_Fisio",SHA1("pass"));
 
 
 /*Creamos las restricciones y los indices para la tabla*/
-ALTER TABLE PEDIDOS ADD UNIQUE (NUMPEDIDO,FECHA,HORA,PAGADO,FISIO)
-CREATE INDEX INDEX_NUMPEDIDO ON PEDIDOS (NUMPEDIDO(10))
+ALTER TABLE PEDIDOS ADD UNIQUE (NUMPEDIDO,FECHA,HORA,PAGADO,FISIO);
+ALTER TABLE `PEDIDOS` ADD INDEX (  `NUMPEDIDO` )
 
